541b0360c579d437caecdf5d0c1f315191994d97
sunada1995 <sunada.takanari1995@email.com>
2025-08-28 10:49:54 +0900

    20250828

--- src/main/java/com/seiken_soft/controller/DutyRegistController.java
+++ src/main/java/com/seiken_soft/controller/DutyRegistController.java
@@ -2,6 +2,8 @@ package com.seiken_soft.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DutyRegistController {
 //勤怠画面用のコントローラー
 //初期表示時はinit()
-	@RequestMapping("")
+	@GetMapping("")
 	public String init(Model model) {
+		return "registDuty";
+	}
+	@PostMapping("/clockinAndClockout")
+	public String stamping(Model model) {
+		
 		return "registDuty";
 	}
 }
