c6b9d2faeba1a2b292cdbee94a68791a99c4e40c
sunada1995 <sunada.takanari1995@email.com>
2025-10-16 11:01:07 +0900

    20251016

--- src/main/java/com/seiken_soft/model/DutyRegistModel.java
+++ src/main/java/com/seiken_soft/model/DutyRegistModel.java
@@ -1,19 +1,9 @@
 package com.seiken_soft.model;
 
-import org.apache.catalina.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Options;
+import com.seiken_soft.entity.TDuty;
 
 public interface DutyRegistModel {
   //勤怠画面用のモデルインターフェース
-	//@Select("SELECT * FROM users WHERE id = #{id}")
-    //User getUserById(Long id);
-    @Insert("INSERT INTO t_duty(name, email) VALUES(#{name}, #{email})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
-    int insertUser(User user);
-    //@Update("UPDATE users SET name = #{name}, email = #{email} WHERE id = #{id}")
-    //int updateUser(User user);
-    //@Delete("DELETE FROM users WHERE id = #{id}")
-    //int deleteUser(Long id);
+	int insert(TDuty record);
 	
 }
--- src/main/java/com/seiken_soft/model/impl/DutyRegistModelImpl.java
+++ src/main/java/com/seiken_soft/model/impl/DutyRegistModelImpl.java
@@ -1,7 +1,21 @@
 package com.seiken_soft.model.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.seiken_soft.dao.TDutyMapper;
+import com.seiken_soft.entity.TDuty;
+
+@Service
 public class DutyRegistModelImpl {
   //勤怠画面用のモデル
+	@Autowired
+	private TDutyMapper tDutyMapper;
 	
+	/* データの挿入 */
+	public void signUp(TDuty tDuty) {
+		
+		tDutyMapper.insert(tDuty);
+	}
 	
 }
